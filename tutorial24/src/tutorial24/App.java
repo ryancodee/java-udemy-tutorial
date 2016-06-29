5623624e23b6ab9f226c83d65ba9a1c78e126dae
Ryan Cruz <ryanjosephcruz1@gmail.com>
2016-06-29 18:18:18 +0800

    Packages tutorial

---
+++ tutorial24/src/tutorial24/App.java
@@ -0,0 +1,18 @@
+package tutorial24;
+
+import ocean.Fish;
+import ocean.plants.Seaweed;
+
+// import every class in that package
+// import ocean.*;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fish fish = new Fish();
+
+		Seaweed weed = new Seaweed();
+	}
+
+}
